16ca83f5b2fcc493b2894893ca78377259cd3ac0
Igor De Almeida Aguiar <102562449+AguiarIgor@users.noreply.github.com>
2023-05-23 14:56:10 -0300

    Add files via upload

---
+++ 2º Bimestre/Responsivo/script.js
@@ -0,0 +1,8 @@
+function ApareceMenu(){
+    if(document.getElementById("lista").style.display == 'none'){
+        document.getElementById("lista").style.display = 'block'
+    }
+    else{
+        document.getElementById("lista").style.display = 'none'
+    }
+}
