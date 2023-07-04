9177a0f6b5737664d81b03abdadffb14a9f2b564
Igor Aguiar <aguiarigor365@gmail.com>
2023-07-04 17:50:44 -0300

    Update DOM - Final

--- 3º Bimestre/DOM/script.js
+++ 3º Bimestre/DOM/script.js
@@ -1,6 +1,20 @@
 document.getElementById("video").style.margin = "0";
 document.getElementById("video").style.zIndex = "-1";
 
-document.getElementById("table").style.textAlign = "center";
-document.getElementById("table").style.borderCollapse = "collapse";
-document.getElementById("table").style.width = "50vw";
+document.getElementsByName("table")[0].style.borderCollapse = "collapse";
+document.getElementsByName("table")[0].style.width = "50vw";
+document.getElementsByTagName("table")[0].style.textAlign = "center";
+
+document.getElementsByTagName("main")[0].style.alignItems = "center";
+document.getElementsByTagName("main")[0].style.display = "flex";
+
+document.getElementsByClassName("none")[0].style.border = "none";
+document.getElementsByClassName("none")[0].style.backgroundColor = "transparent";
+
+document.querySelector("th.coluna1").style.color = "red";
+document.querySelector("th.coluna2").style.color = "green";
+
+/*
+document.querySelector("footer#rodape").style.padding = "20px"
+document.querySelector("div.container-item").style.padding = "10px"
+*/
