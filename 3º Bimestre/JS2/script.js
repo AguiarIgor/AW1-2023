0267131c1c6157d55b1f8094cd57ded582e855cf
Igor Aguiar <aguiarigor365@gmail.com>
2023-07-04 16:56:11 -0300

    Update DMO - Final

---
+++ 3º Bimestre/JS2/script.js
@@ -0,0 +1,6 @@
+document.getElementById("video").style.margin = "0";
+document.getElementById("video").style.zIndex = "-1";
+
+document.getElementById("table").style.textAlign = "center";
+document.getElementById("table").style.borderCollapse = "collapse";
+document.getElementById("table").style.width = "50vw";
